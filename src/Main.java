fc9c006fe9e70b124f17bebe5af36a0dec9caba1
Mahesa Fadhillah Andre <167867383+mahesa005@users.noreply.github.com>
2025-02-19 21:35:04 +0700

    chore: setup structure

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan file path: ");
+        String filename = scanner.nextLine();
+        String filepath = "test/input/" + filename;
+        
+        try {
+            List<String> inputList = Files.readAllLines(Paths.get(filepath));
+            for (String line : inputList) {
+                System.out.print(line);
+            }
+        }
+        catch (IOException e) {
+            System.out.println("Terjadi kesalahan: " + e.getMessage());
+        }
+        scanner.close();
+    }
+}
